b1f8e01810f48b1d97ef0fc76563260b2536d522
Danish khan <muhammaddanishkhan1999@gmail.com>
2021-12-18 23:03:44 +0500

    My first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer id="sticky-footer" className="bg-light text-dark-50">
+      <div className="container py-1">
+        <div className="row"></div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
